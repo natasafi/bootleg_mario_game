634e3e4e79ca9c45ca5eed0213a28ced50cac153
natasafi <natasafrag@gmail.com>
2023-04-24 19:55:38 +0100

    creates the game screen

---
+++ main.py
@@ -0,0 +1,35 @@
+import os
+import random
+import math
+import pygame
+
+from os import listdir
+from os.path import isfile, join
+
+
+pygame.init()
+
+pygame.display.set_caption("Bootleg Mario")
+
+BACKGROUND_COLOUR = (255, 255, 255)
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+PLAYER_VELOCITY = 5
+
+window =  pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            break
+
+
+if __name__ == "__main__":
+    main(window)
